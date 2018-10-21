5de65c4c4b5647be2ef539f975a85fbe200adcb3
Dawidson001 <dawidson01@interia.pl>
2018-10-21 13:38:20 +0200

    Implementacja

--- src/com/sda/git/Main.java
+++ src/com/sda/git/Main.java
@@ -1,8 +1,29 @@
 package com.sda.git;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
+        //////Array
+
+        List<String>list = new LinkedList<>();
+
+        list.add("Jan");
+        list.add("Marcin");
+        list.add("Karol");
+
+        for (String l:list)
+        {
+            System.out.println(l);
+        }
+
+
+
+
+
     }
 }
